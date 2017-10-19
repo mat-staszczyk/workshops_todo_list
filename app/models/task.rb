d5e0a8a61811663f92035cfa726e54d73f75e059
sleaz0id <mat.staszczyk@gmail.com>
2017-10-19 19:44:16 +0200

    Add deadline field

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+
+  def self.ending_first
+    order :deadline
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   subject { Task.new }
   it { is_expected.to validate_presence_of(:title) }
+
+  context 'ending_first' do
+    it 'should have right order' do 
+      task1 = Task.create!(title: 'Nope task')
+      task2 = Task.create!(title: 'Urgent task', deadline: DateTime.now)
+      expect(Task.ending_first.to_a).to eq([task2, task1])
+    end
+  end 
 end
